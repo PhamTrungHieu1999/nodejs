1e5a95ec1f9161dcef93112408d106617b5dec6a
Phạm Hiếu <phamhieukmen199999@gmail.com>
2022-06-10 22:49:37 +0700

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-var handlebars = require("express-handlebars");
+const { engine } = require("express-handlebars");
 const port = 3000;
+app.use(express.static(path.join(__dirname, 'public')))
+// http logger
 app.use(morgan("combined"));
 //template engine để cấu trúc code hợp lý
 //chia file thành các view, file layout code áp dụng cho các trang web
 //cấu trúc file thành mô hình mvc(model, view, controller)
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'resources/views'))
+app.engine("hbs", engine({
+  extname: '.hbs'
+}));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 app.get("/", (req, res) => {
-    res.render('home');
+  res.render("home");
+});
+app.get('/news', (req, res) => {
+    res.render('news')
 });
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
