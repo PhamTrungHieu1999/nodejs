d417730404e8f093d53bef4b0f83174f37fe309e
Phạm Hiếu <phamhieukmen199999@gmail.com>
2022-06-12 16:31:08 +0700

    controller router

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ const morgan = require("morgan");
 const app = express();
 const { engine } = require("express-handlebars");
 const port = 3000;
-app.use(express.static(path.join(__dirname, 'public')))
+const route = require('./routes');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+//XMLHttlRequest, fetch, axios, để gửi yêu cầu lên server
 // http logger
 app.use(morgan("combined"));
 //template engine để cấu trúc code hợp lý
@@ -15,12 +21,8 @@ app.engine("hbs", engine({
 }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get('/news', (req, res) => {
-    res.render('news')
-});
+//HOme, search, contact không biết nên đưa vào controller nào cách giải quyết đưa vào 1 file
+route(app);
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
